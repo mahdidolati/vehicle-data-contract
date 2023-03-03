6dd99cf8de56a60540ceadc300a010f85c96f0da
Mahdi Dolati <mehdidolati@hotmail.com>
2023-03-03 20:49:31 +0330

    use oo entities in a new main

---
+++ main2.py
@@ -0,0 +1,23 @@
+from solcx import install_solc
+from car import Car
+
+def run():
+    # a public directory to map cars to contracts
+    contract_db = dict()
+
+    car_id = 0
+    car = Car(car_id)
+    car_id = car_id + 1
+    car.init()
+    contract_db[car.id] = car.abi
+    car.use()
+    car.read()
+
+    # insurance1 = InsuranceCompany()
+    # car_abi = contract_db[car.id]
+
+
+if __name__ == "__main__":
+    install_solc('0.6.0')
+    run()
+        
